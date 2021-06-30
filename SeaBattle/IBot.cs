ea6710ae427b5905e1c6106f35d3e73106fa818b
demon.d.98 <demon.d.98@mail.ru>
2021-06-30 14:10:13 +0500

    Добавьте файлы проекта.

---
+++ SeaBattle/IBot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattle
+{
+	public interface IBot
+	{
+		bool[,] CreateStartPos(int fieldSize, IEnumerable<(int length, int count)> ships);
+
+		(int x, int y) MakeStep(Cell[,] field)
+	}
+}
